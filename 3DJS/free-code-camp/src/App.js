cb38260ddb7e58b172b61617a504d9a437ed15ea
Hendrik Geiger <g9a2hvk9@gmail.com>
2021-12-24 13:45:37 +0100

    finishes 'Loading Data in React' section

--- 3DJS/free-code-camp/src/App.js
+++ 3DJS/free-code-camp/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { csv, csvFormat } from 'd3';
 import './App.css';
+
+
+const csvUrl = 'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv'; 
+
 
 function App() {
 
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+      csv(csvUrl).then(setData);
+  }, [])
+
+  const message = data => {
+    let message = '';
+    message = message + Math.round(csvFormat(data).length / 1024) + ' kb\n';
+    message = message + data.length + ' rows\n';
+    message = message + data.columns.length + ' columns';
+    return message
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Loading data with D3</h1>>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Using D3 with React</h1>
+    <pre>{data ? message(data) : 'Loading...'}</pre>
     </div>
   );
 }
